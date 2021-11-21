db2d9dff532bb174ec1037399a7b1762d381031a
mengrru <mengrru@outlook.com>
2021-11-21 14:16:31 +0800

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,24 +18,28 @@ const initPlugin = async () => {
     pageUrl: 'panel.html',
     maxHeight: 430,
   })
-  /*
   Event.sub(Event.type.CreateFrame, async (items) => {
     for (const frame of items) {
       Action.CreateATagForACleanFrame(
+        {
+          height: 30,
+          width: 130,
+          x: frame.x + frame.width/2 - 130 / 2,
+          y: frame.y - frame.height / 2 + 30 / 2,
+          style: {
+            fillColor: '#ccc'
+          }
+        },
         frame, Tags.State, 'todo'
       )
     }
   })
-  */
-  /*
   Event.sub(Event.type.DeleteFrame, async (m) => {
     for (const frame of m) {
-      Action.DeleteFrame(
-        frame
-      )
+      await Action.DeleteATagFromAFrame(frame, Tags.State)
+      await Action.DeleteATagFromAFrame(frame, Tags.Owner)
     }
   })
-  */
 
   //调试用
   /*
--- frontend/src/panel/index.jsx
+++ frontend/src/panel/index.jsx
@@ -23,6 +23,7 @@ const App = () => {
     })
   }
   return (
+
     <div className="app">
       <div className='header'></div>
       <div className='tags'>
